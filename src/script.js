9b82c853a4d3415095ead115474bbe3800dd527c
Ramita <104021517+ramitaarora@users.noreply.github.com>
2024-07-07 14:58:40 -0700

    quiz starting and restarting

--- src/script.js
+++ src/script.js
@@ -1,3 +1,32 @@
-const questions = require('./questionData.json');
 const questionEl = document.getElementById('question');
 const answerEl = document.getElementById('answer');
+const startButton = document.getElementById('start');
+const answerButton = document.getElementById('answer');
+const nextButton = document.getElementById('next');
+const restartButton = document.getElementById('restart');
+
+const finishedQuestions = [];
+
+// Import questions
+
+const response = await fetch('./src/questionData.json');
+const questions = await response.json();
+
+startButton.addEventListener('click', () => {
+    // Set buttons visibility
+    questionEl.setAttribute('class', 'visible');
+    startButton.setAttribute('class', 'hidden');
+    nextButton.setAttribute('class', 'visible');
+    restartButton.setAttribute('class', 'visible');
+
+    const index = Math.floor(questions.length * Math.random());
+
+    let thisQuestion = document.createElement('h3')
+    thisQuestion.innerText = questions[index].question;
+    questionEl.append(thisQuestion);
+    }
+)
+
+restartButton.addEventListener('click', () => {
+   location.reload();
+})
